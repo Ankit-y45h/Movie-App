747d7e82884e14036f6fb816ed2fa35ef38a4ae3
Ankit-y45h <ankityash03@gmail.com>
2024-07-07 16:24:34 +0530

    completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,39 +1,51 @@
-import React from "react";
-import { addFavourite, removeFromFavourite } from "../actions";
+import React, { Component } from 'react';
+import { addToFavourites, removeFromFavourites } from '../actions';
 
-class MovieCard extends React.Component{
-    
-    handleFavouriteClick = () => {
-        const {movie} = this.props;
-        this.props.dispatch(addFavourite(movie))
-    }
-    handleUnFavouriteClick = () => {
-        const {movie} = this.props;
-        this.props.dispatch(removeFromFavourite(movie))
-    }
-    render (){
-        const {movie, isFavourite} = this.props;
-        return(
-            <div className="movie-card">
-                <div className="left">
-                    <img alt="movie-poster" src={movie.Poster}/>
-                </div>
-                <div className="right">
-                    <div className="title">{movie.Title}</div>
-                    <div className="plot">{movie.Plot}</div>
-                    <div className="footer">
-                        <div className="rating">{movie.imdbRating}<img alt="star" className="rating-star" src="https://cdn-icons-png.flaticon.com/128/1828/1828961.png"/></div>
-                        {
-                            isFavourite
-                            ? <button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}>Unfavourite</button>
-                            : <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>
-                        }
-                        
-                    </div>
-                </div>
-            </div>
-        );
-    }
+class MovieCard extends Component {
+  handleFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(addToFavourites(movie));
+  };
+
+  handleUnFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavourites(movie));
+  };
+
+  render() {
+    const { movie, isFavourite } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="left">
+          <img src={movie.Poster} alt="movie-pic" />
+        </div>
+        <div className="right">
+          <div className="title">
+            {movie.Title} ({movie.Year})
+          </div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}<img alt='stars' className='rating-star' src='https://cdn-icons-png.flaticon.com/128/2893/2893811.png'/></div>
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavouriteClick}
+              >
+                Unfavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavouriteClick}
+              >
+                Favourite
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
